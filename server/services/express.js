4471099438061031f8e698ef5c6de0fc634982cf
anakin-dabir <anakindabir@gmail.com>
2024-04-06 08:57:53 +0500

    fixinf_cors_eror

--- server/services/express.js
+++ server/services/express.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "https://hotel-ten-livid.vercel.app/admin",
+    origin: "https://hotel-ten-livid.vercel.app/",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   })
