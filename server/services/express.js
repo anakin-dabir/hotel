985388db933cdb6949c6625c1397c5359cb7e2b5
anakin-dabir <anakindabir@gmail.com>
2024-04-06 08:09:47 +0500

    adding+_cors_origin

--- server/services/express.js
+++ server/services/express.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["https://hotel-pearl-three.vercel.app/"],
+    origin: "https://hotel-pearl-three.vercel.app/",
     credentials: true,
   })
 );
