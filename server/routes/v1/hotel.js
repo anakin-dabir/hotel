a4a2108a473c2920d6b9c9e2f829b1fe682cd914
anakin-dabir <anakindabir@gmail.com>
2024-05-15 15:35:14 +0500

    Fix: Some unecessary changes

--- server/routes/v1/hotel.js
+++ server/routes/v1/hotel.js
@@ -14,6 +14,7 @@ import verifyToken from "../../middleware/verifyToken.js";
 const hotelRouter = express.Router();
 
 hotelRouter.get("/:hotelId", get);
+
 hotelRouter.get("/rooms/:hotelId", verifyToken, getRoomsByHotelID);
 
 // authentication
